cddeac7a73e61325d8f95608f754f0ee3a95a439
Ada Sofie Amlie <adasam@ulrik.uio.no>
2022-09-28 12:03:29 +0200

    Create Hovedprogram.java

---
+++ Oblig 2/Hovedprogram.java
@@ -0,0 +1,37 @@
+public class Hovedprogram{
+
+    public static void main(String[] args){
+        Narkotisk nark = new Narkotisk("nark", 10, 15.5, 4);
+        Vanlig vanlig = new Vanlig("vanlig", 20, 50.2);
+        Vanedannende vaned = new Vanedannende("vaned", 110, 30.5, 3);
+
+        System.out.println("");
+        System.out.println(nark.toString());
+        System.out.println(vanlig.toString());
+        System.out.println(vaned.toString());
+        System.out.println("");
+
+        Lege lege = new Lege("Lege-navn");
+        Spesialist spesL = new Spesialist("Spes-Lege-navn", "kontrollID-12");
+
+        System.out.println(lege.toString());
+        System.out.println(spesL.toString());
+        System.out.println(spesL.hentKontrollId());
+        System.out.println("");
+
+        Militaerresepter mil = new Militaerresepter(nark, spesL, 23, 5);
+        PResepter pr = new PResepter(vaned, lege, 15, 6);
+        BlaaResepter blaa = new BlaaResepter(vanlig, lege, 19, 8);
+
+        System.out.println(mil.toString());
+        System.out.println("");
+
+        System.out.println(pr.toString());
+        System.out.println("");
+
+        System.out.println(blaa.toString());
+        System.out.println("");
+
+
+    }
+}
